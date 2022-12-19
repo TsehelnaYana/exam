d3ceea343e6f98b05c23c7bd6b1b9557ab08ee70
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-19 13:48:15 +0200

    +

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import datetime
+from datetime import date
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-  def progress(n):
-    d = 3
-    a = 2
-    return a + n * d
+def hello_world():
+    today = datetime.datetime.now()
+    bday = datetime.datetime(2023,12,16,14,45)
+    time_diff = bday - today 
+    return('Tsehelna Yana.Your birthday is in ' + str(time_diff))
 
-if __name__ =='__main__':
-    app.run(debug=True,host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
