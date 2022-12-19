fd68b69a8e52a8e2a8d5d7174a8c4fdf112f058f
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-19 14:03:13 +0200

    +

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 import unittest
-import app as tested_app
+import app as test_app
 import json
 
 class FlaskAppTests(unittest.TestCase):
 
-    def setUp(self):
-        self.app = tested_app.app.test_client()
+	def setUp(self):
+		self.app = test_app()
 
-    def test_get_hello_endpoint(self):
-        r = self.app.get('/')
-        self.assertEqual(r._status_code, 200)
-        
+	def test_get_hello_endpoint(self):
+		r = self.app.get('/')
+		self.assertEqual(r.status, '200 OK')
+		self.assertEqual(r.data, 157)
+		self.assertIsNotNone(r.data)
+		self.assertGreater(r.data, 0)
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ =='__main__':
+	unittest.main()
