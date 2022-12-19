68370fb49f4795c968bdc679e46a8ed9e33e4c6a
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-19 12:55:09 +0200

    +

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    today = datetime.datetime.now()
-    bday = datetime.datetime(2023,12,16,14,45)
-    time_diff = bday - today 
-    return('Tsehelna Yana.Your birthday is in ' + str(time_diff))
+x=2
+sumx=0
+print('Введіть число n')
+for i in range(int(input())):
+    sumx+=x
+    x+=3
+print('Сума n перших членів арифметичної прогресії х=2, 5, 8...: ' + str(sumx))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
