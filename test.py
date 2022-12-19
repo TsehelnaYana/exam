e91c869d4422b370da52916afe86c65832791871
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-19 12:57:36 +0200

    +

--- test.py
+++ test.py
@@ -4,19 +4,18 @@ import json
 
 class FlaskAppTests(unittest.TestCase):
 
-    def setUp(self):
-        #tested_app.app.config['TESTING'] = True
-        self.app = tested_app.app.test_client()
+	def setUp(self):
+		self.app = tested_app.test_client()
 
-    def test_get_hello_endpoint(self):
-        r = self.app.get('/')
-        self.assertEqual(r.status, '200 OK')
-       
+	def test_get_hello_endpoint(self):
+		r = self.app.get('/')
+		self.assertEqual(r.status, '200 OK')
+		self.assertEqual(r.data, 157)
+		self.assertIsNotNone(r.data)
+		self.assertGreater(r.data, 0)
 
-if __name__ == '__main__':
-
-    import xmlrunner 
-    runner = xmlrunner.XMLTestRunner(output='test-reports')
-    unittest.main(testRunner=runner)
-
-    unittest.main()
+if __name__ =='__main__':
+	import xmlrunner
+	runner = xmlrunner.XMLTestRunner(output='test-reporst')
+	unittest.main(testRunner=runner)
+	unittest.main()
