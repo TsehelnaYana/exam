8a7033d259e63b0f4f15323d5b0556d91e29ca14
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-19 14:02:34 +0200

    +

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
 def hello_world():
     n = 5
     x = 2
     for i in range(n):
         x = x+x+3
     return x
+
+if __name__ =='__main__':
+    app.run(debug=True,host='0.0.0.0')
